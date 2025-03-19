6fe5363f94c522de95675926a2e550de521d1cd2
tiffanyyong <G00425067@atu.ie>
2025-03-19 09:40:14 +0000

    css for timetable

--- src/components/BusTimetable.js
+++ src/components/BusTimetable.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const BusTimetable = () => {
   const buses = [
@@ -30,10 +33,104 @@ const BusTimetable = () => {
       cheapest: true,
       quickest: true,
     },
+    {
+      id: 3,
+      number: "712",
+      route: "towards: Cork",
+      departureTime: "22:15",
+      departureLocation: "Zone 11, Dublin Airport",
+      arrivalTime: "01:45",
+      arrivalLocation: "Cork Bus Station",
+      arrivalDate: "19/03/2025",
+      journeyTime: "3h 30m",
+      price: 20.00,
+      cheapest: false,
+      quickest: false
+    },
+    {
+      id: 4,
+      number: "712",
+      route: "towards: Cork",
+      departureTime: "23:45",
+      departureLocation: "Zone 11, Dublin Airport",
+      arrivalTime: "03:15",
+      arrivalLocation: "Cork Bus Station",
+      arrivalDate: "19/03/2025",
+      journeyTime: "3h 30m",
+      price: 20.00,
+      cheapest: false,
+      quickest: false
+    },
+    {
+      id: 5,
+      number: "740",
+      route: "towards: Limerick",
+      departureTime: "21:30",
+      departureLocation: "Zone 11, Dublin Airport",
+      arrivalTime: "00:30",
+      arrivalLocation: "Limerick Bus Station",
+      arrivalDate: "19/03/2025",
+      journeyTime: "3h 0m",
+      price: 18.00,
+      cheapest: true,
+      quickest: false
+    },
+    {
+        "id": 6,
+        "number": "740",
+        "route": "towards: Limerick",
+        "departureTime": "23:10",
+        "departureLocation": "Zone 11, Dublin Airport",
+        "arrivalTime": "02:00",
+        "arrivalLocation": "Limerick Bus Station",
+        "arrivalDate": "19/03/2025",
+        "journeyTime": "2h 50m",
+        "price": 18.00,
+        "cheapest": true,
+        "quickest": true
+    },
+    {
+        "id": 7,
+        "number": "820",
+        "route": "towards: Waterford",
+        "departureTime": "20:45",
+        "departureLocation": "Zone 11, Dublin Airport",
+        "arrivalTime": "23:30",
+        "arrivalLocation": "Waterford Bus Station",
+        "arrivalDate": "19/03/2025",
+        "journeyTime": "2h 45m",
+        "price": 16.00,
+        "cheapest": true,
+        "quickest": true
+    },
+    {
+        "id": 8,
+        "number": "820",
+        "route": "towards: Waterford",
+        "departureTime": "22:30",
+        "departureLocation": "Zone 11, Dublin Airport",
+        "arrivalTime": "01:15",
+        "arrivalLocation": "Waterford Bus Station",
+        "arrivalDate": "19/03/2025",
+        "journeyTime": "2h 45m",
+        "price": 16.00,
+        "cheapest": true,
+        "quickest": true
+    }
   ];
 
   return (
+    
     <div className="bus-list">
+      <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Nav className="me-auto">
+              <Nav.Link href="/register">Register</Nav.Link>
+              <Nav.Link href="/timetable">Timetable</Nav.Link>
+              <Nav.Link href="/camera">Ticket</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
       {buses.map((bus) => (
         <div key={bus.id} className="bus-container">
           <div className="bus-header">
@@ -50,7 +147,7 @@ const BusTimetable = () => {
 
           <div className="bus-icons">📶 🔄 🌿</div>
 
-          <button className="bus-button">Choose outbound</button>
+          <button className="bus-button">Choose</button>
 
           <div className="bus-badges">
             {bus.cheapest && <span className="badge cheapest">CHEAPEST</span>}
