eb6f7738b79e90401fbd918aa8aff89f1753c33a
tiffanyyong <G00425067@atu.ie>
2025-04-02 21:27:06 +0100

    design home page nav bar

--- src/components/user_menu.js
+++ src/components/user_menu.js
@@ -4,27 +4,45 @@ import Navbar from 'react-bootstrap/Navbar';
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import '../css/Header.css';
+import { FaBus, FaUserCircle, FaSearch } from 'react-icons/fa';
 
 const UserMenu = () => {
   const navigate = useNavigate();
 
     return (
-    <div>
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Nav className="me-auto">
-              <Nav.Link href="/register">Register</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/search">Timetable</Nav.Link>
-            </Nav>
-          </Container>
-      </Navbar>
       <div className="header-container">
       {/* Background Video */}
       <video autoPlay loop muted playsInline className="header-video">
         <source src="/videos/Header.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      {/* Navbar integrated into the video */}
+      <Navbar expand="lg" className="video-navbar">
+        <Container>
+          <Navbar.Brand href="/" className="d-flex align-items-center">
+            <FaBus className="me-2 navbar-brand-icon" />
+            <span className="easyride-brand">EasyRide</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggle" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/register" className="nav-link-custom">
+                <FaUserCircle className="me-1" />
+                Register
+              </Nav.Link>
+              <Nav.Link href="/login" className="nav-link-custom">
+                <FaUserCircle className="me-1" />
+                Login
+              </Nav.Link>
+              <Nav.Link href="/search" className="nav-link-custom">
+                <FaSearch className="me-1" />
+                Timetable
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
       {/* Overlay Content */}
       <div className="header-overlay">
@@ -42,9 +60,7 @@ const UserMenu = () => {
         </div>
       </div>
     </div>
-    </div>
-      
   );
-  };
-  
-  export default UserMenu;
+};
+
+export default UserMenu;
